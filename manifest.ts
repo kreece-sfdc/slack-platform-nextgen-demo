143ebc625f22ea07f4192f87de7e880b249d7fe7
Kevin Reece <kreece@kreece-ltmhgk9.internal.salesforce.com>
2026-06-29 15:16:00 -0700

    Rename app to 'World Cup 2026 (NextGen)'

--- manifest.ts
+++ manifest.ts
@@ -1,6 +1,7 @@
 import { Manifest } from "deno-slack-sdk/mod.ts";
 import AppHomeWorkflow from "./workflows/app_home_workflow.ts";
 import MessageWorkflow from "./workflows/message_workflow.ts";
+import MentionWorkflow from "./workflows/mention_workflow.ts";
 import UsersDatastore from "./datastores/users_datastore.ts";
 import { AiGetGamesDefinition } from "./functions/ai_get_games.ts";
 import { AiGetStandingsDefinition } from "./functions/ai_get_standings.ts";
@@ -8,10 +9,10 @@ import { AiGetBracketDefinition } from "./functions/ai_get_bracket.ts";
 import { AiGetPlayersDefinition } from "./functions/ai_get_players.ts";
 
 export default Manifest({
-  name: "World Cup 2026",
+  name: "World Cup 2026 (NextGen)",
   description: "Follow your team's fixtures, standings, bracket, and squad",
   icon: "assets/icon.png",
-  workflows: [AppHomeWorkflow, MessageWorkflow],
+  workflows: [AppHomeWorkflow, MessageWorkflow, MentionWorkflow],
   functions: [
     AiGetGamesDefinition,
     AiGetStandingsDefinition,
